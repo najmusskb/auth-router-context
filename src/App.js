edaa7b50fcc7d2164c217cbd7fa7efc44494745b
unknown <najmussakib173@gmail.com>
2022-10-27 14:04:34 +0600

    auth context

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './Component/Home/Home';
+import Login from './Component/Login/Login';
+import Register from './Component/Register/Register';
+import Main from './layout/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Main></Main> ,
+
+      children: [
+        {
+          path:'/',
+          element:<Home></Home>
+        },
+        {
+          path:'/login',
+          element:<Login></Login>
+        },
+        {
+          path:'/register',
+          element:<Register></Register>
+        }
+      ]
+    }
+    
+
+
+
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <RouterProvider router={router}></RouterProvider>
+ 
     </div>
   );
 }
-
 export default App;
